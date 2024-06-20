f912c27bf509b29a1e4f8c9e66878fbd72f2e4c8
Rizwan9498 <rizwanb.tech94@gmail.com>
2024-06-20 11:54:14 +0530

    Add files via upload

---
+++ python 12.06.2024 afternoon.py
@@ -0,0 +1,23 @@
+s='tution teacher'
+print(s)
+print(type(s))
+print(s.upper())
+print(s.lower())
+print(s.islower())
+print(s.capitalize())
+
+m='noorul islam university'
+print(m)
+print(type(m))
+
+swap='swapping lettes'
+print(swap.swapcase())
+print(len(swap))
+print('maximum values is',max(swap))
+print(swap)
+print('minimum value is',min(swap))
+print(swap)
+      
+print(ord('A'))
+print(ord('C'))
+print(ord('d'))
